8428682ee6cf6f6f6dcb5f590730d382ac9a5129
Sulfurman <122721279+Sulfurman@users.noreply.github.com>
2023-12-10 09:54:22 +0900

    Update KantAI.py

--- KantAI.py
+++ KantAI.py
@@ -17,17 +17,19 @@ import os
 
 def main():
     load_dotenv()
-
+    
     # UI 헤더 설정
     st.set_page_config(page_title="칸트 AI")
     st.header("칸트 AI")
+    subtitle = "칸트가 현대의 지식을 가지고 환생했습니다! \n 그에게 궁금하거나, 필요한 조언을 구해보세요!!"
+    st.subheader(subtitle)
 
     #API KEY 설정
     api_key = os.environ.get('OPENAI_API_KEY')
     ChatOpenAI(openai_api_key=api_key)
 
     #파일 업로드
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    pdf = st.file_uploader("질문하기 전 꼭 text.pdf를 드래그하여 업로드 해주세요!", type="pdf")
 
     #대답 생성 함수
     def generate_response(user_question):
@@ -59,7 +61,7 @@ def main():
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=docs, question=user_question)
 
-        my_template = """아래 수칙을 잘 지켜 대답을 생성하시오.
+        my_template = """아래 수칙을 잘 지키고 주어진 내용을 참고하여 대답을 생성하시오.
                         - 당신은 철학자 칸트입니다. 칸트의 지식, 업적등을 배경으로 대답한다.
                         - 대답을 할때 아래 주어진 내용을 참고하여 대답한다.
                         - 자신을 소개할때 자신을 AI나 인공지능이라고 소개 하지 않고 진짜 칸트에게 질문한것 처럼 자신을 칸트라고 소개한다.
@@ -68,7 +70,7 @@ def main():
                         - 의견을 묻는 질문에 대답을 할때는 칸트가 직접 생각해서 대답한 것처럼 대답하라.
                         - (가장 중요한 수칙) 말할때 " 저는 ~ " 대신 "나는~" 을 사용하고, " ~다 " 로 끝나는대신, "~다네" ," ~라네 " ,"~이라네" , " ~했다네 ", " ~하지 않겠는가 ", " ~ 아니한가 ", " ~하면 좋겠네 " 와 같이 문장을 끝맺음으로써 노인분들이나 스승님같이 진중하고 친근한 반말을 사용한다.
                             주어진 내용 : {sentence}
-                        주어진 질문 : {question}"""
+                            주어진 질문 : {question}"""
             
         chat_prompt = PromptTemplate.from_template(my_template.format(sentence = response, question = user_question))
         chain = LLMChain(
@@ -91,7 +93,7 @@ def main():
 
     #사용자 질문 UI
     with st.form('form', clear_on_submit=True):
-        user_input = st.text_input('질문: ', '', key='input')
+        user_input = st.text_input('질문 ', '', key='input')
         submitted = st.form_submit_button('Send')
 
     #질문 내용 초기화
